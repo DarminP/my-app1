ac0a18c118023c37f34e1e9cdca39206f9a641c1
Darmin Poturovic <d.poturovic@gmail.com>
2021-11-10 19:51:08 +0100

    update2

--- src/books.js
+++ src/books.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import axios from 'axios';
 
 class Books extends React.Component
 {
+    state={
+        bookList:[]
+    }
+    componentDidMount()
+    {
+        axios.get('http://localhost:20588/api/Books').then(res=>
+        {
+            console.log(res.data)
+            this.setState({
+                bookList:res.data
+            })
+        })
+    }
     render()
     {
         return(
             <div>
-                <h1>Books</h1>
-            </div>
-        )
+                <h1 className="header">Books</h1>
+                <table className="center">
+                    <tbody>
+                        <tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                        </tr>
+                        {this.state.bookList.map(book=>{
+                            return(
+                                <tr>
+                                    <td>{book.title}</td>
+                                    <td>{book.author}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                </div>
+            )
+        }
     }
-}
-export default Books;
+    export default Books;
+               
